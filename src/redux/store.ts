5e80fc244dc8a232ddeaccf6fc3c132a40c7226c
PHUC-A2 <phucban297@gmail.com>
2025-11-30 04:16:43 +0700

    #10 delete file counterSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './features/counterSlice'
 import authReducer from './features/authSlice'
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         auth: authReducer,
     }
 })
