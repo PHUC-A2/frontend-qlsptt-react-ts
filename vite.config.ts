60e8eba6556b71d82600b83173283393c1c401b9
PHUC-A2 <phucban297@gmail.com>
2025-12-26 18:11:14 +0700

    #1 chat messager fb and demo deploy ngrok free

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true
+    open: true,
+    port: 3000,
+    allowedHosts: [
+      'unvigorous-nondeleteriously-mandi.ngrok-free.dev'
+    ],
   }
 })
